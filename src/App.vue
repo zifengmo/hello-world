6415542872f6da9b4ce2cc5c7c1822c8fc2e8f54
wangyd <wangyd@tuya.com>
2020-04-27 10:35:28 +0800

    爱你哟

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/WechatIMG14.jpeg">
     <HelloWorld msg="幸得余生，与你相拥"/>
   </div>
 </template>
